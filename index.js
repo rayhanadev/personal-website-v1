37817e9d2b96f9a18d3e8405b6410b43af91a3c3
RayhanADev <72509475+RayhanADev@users.noreply.github.com>
2020-12-09 07:56:01 -0600

    Minor Data Changes

--- index.js
+++ index.js
@@ -4,66 +4,109 @@ const ejs = require('ejs');
 
 const repl = require('replapi-it');
 
+const fs = require('fs');
+
+const fetch = require('node-fetch');
+
 const _debug = false;
 const _port = 3000;
 
+const _fetchrate = 5;
+
+const _server = true;
+
 let user = new repl.User('RayhanADev');
 
-let data;
+let data = {
+	profile: JSON.parse(fs.readFileSync('backups/profilebackup.txt')),
+	posts: JSON.parse(fs.readFileSync('backups/postbackup.txt')),
+	comments: JSON.parse(fs.readFileSync('backups/commentbackup.txt')),
+	github: JSON.parse(fs.readFileSync('backups/githubbackup.txt'))
+};
 
 setInterval(async function() {
-  try {
-    let profile = await user.profileData();
-    console.log('[DEBUG] ~ Got Profile Data');
-    
-    let posts = await user.postData('', 10000);
-    posts = posts.length;
-    console.log('[DEBUG] ~ Got Post Data');
-    
-    let comments = await user.commentData('', 10000);
-    comments = comments.length;
-    console.log('[DEBUG] ~ Got Comment Data');
-    
-    data = { profile: profile, posts: posts, comments: comments };
-  } catch(err) {
-    console.log('[ERROR] ~ Fetching Profile Data')
-  }
-}, 1000 * 10);
+	try {
+		let profile = await user.profileData();
+		console.log('[DEBUG] ~ Got Profile Data');
+		fs.writeFileSync('backups/profilebackup.txt', JSON.stringify(profile));
 
-app.use(express.static('views'));
-app.use(express.static('public'));
+		let posts = await user.postData('', 10000);
+		posts = { posts: posts.length };
+		console.log('[DEBUG] ~ Got Post Data');
+		fs.writeFileSync('backups/postbackup.txt', JSON.stringify(posts));
 
-app.set('view engine', 'ejs');
+		let comments = await user.commentData('', 10000);
+		comments = { comments: comments.length };
+		console.log('[DEBUG] ~ Got Comment Data');
+		fs.writeFileSync('backups/commentbackup.txt', JSON.stringify(comments));
 
-app.get('/', (req, res) => {
-	if (data == undefined) res.redirect('/error/dataless');
-	if (data.profile == undefined) res.redirect('/error/dataless');
-	if (data.posts == undefined) res.redirect('/error/dataless');
-	if (data.comments == undefined) res.redirect('/error/dataless');
+		let github = await fetch('https://api.github.com/users/rayhanadev', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				Connection: 'keep-alive',
+				'X-Requested-With': 'https://ray.is-a.dev'
+			}
+		}).then(res => res.json());
+		fs.writeFileSync('backups/githubbackup.txt', JSON.stringify(github));
 
-	if (_debug) console.log('[DEBUG] ~ Viewed Page (Main)');
-	res.render('index', { data: data });
-});
-
-app.get('/error/:error', (req, res) => {
-	let error;
-	if (req.params.error == 'dataless') {
-		error = {
-			title: 'No Data',
-			msg: "We haven't gotten my data queried. Oops! Check back in a couple of minutes"
+		data = {
+			profile: profile,
+			posts: posts,
+			comments: comments,
+			github: github
 		};
-	} else {
-		res.redirect('/404');
+	} catch (err) {
+		console.log('[ERROR] ~ Fetching Profile Data, using backup!');
+		console.log(err.message);
+
+		data = {
+			profile: JSON.parse(fs.readFileSync('backups/profilebackup.txt')),
+			posts: JSON.parse(fs.readFileSync('backups/postbackup.txt')),
+			comments: JSON.parse(fs.readFileSync('backups/commentbackup.txt')),
+			github: JSON.parse(fs.readFileSync('backups/githubbackup.txt'))
+		};
 	}
+}, 60000 * _fetchrate);
 
-	res.render('error', { error: error });
-});
+if (_server) {
+	app.use(express.static('views'));
+	app.use(express.static('public'));
 
-app.use((req, res) => {
-	res.status('404');
-	res.render('404');
-});
+	app.set('view engine', 'ejs');
 
-app.listen(_port, () => {
-	console.log(`[SERVER] ~ Server listening on Port: ${_port}`);
-});
+	app.get('/', (req, res) => {
+		if (data == undefined) res.redirect('/error/dataless');
+		if (data.profile == undefined) res.redirect('/error/dataless');
+		if (data.posts == undefined) res.redirect('/error/dataless');
+		if (data.comments == undefined) res.redirect('/error/dataless');
+
+		if (_debug) console.log('[DEBUG] ~ Viewed Page (Main)');
+		res.render('index', { data: data });
+	});
+
+	app.get('/error/:error', (req, res) => {
+		let error;
+		if (req.params.error == 'dataless') {
+			error = {
+				title: 'No Data',
+				msg:
+					"We haven't gotten my data queried. Oops! Check back in a couple of minutes"
+			};
+		} else {
+			res.redirect('/404');
+		}
+
+		res.render('error', { error: error });
+	});
+
+	app.use((req, res) => {
+		res.status('404');
+		res.render('404');
+	});
+
+	app.listen(_port, () => {
+		console.log(`[SERVER] ~ Server listening on Port: ${_port}`);
+	});
+}
